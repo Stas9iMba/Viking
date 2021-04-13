0d599a3b1fb1ac565a57d729802597c74bbdd17c
Stas <46508483+Stas9iMba@users.noreply.github.com>
2021-04-14 00:19:14 +0700

    done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 $(function () {
-  $('[data-fancybox]').fancybox({
-    youtube: {
-      controls: 0,
-      showinfo: 0,
-    },
-  });
+  // $('[data-fancybox]').fancybox({
+  //   youtube: {
+  //     controls: 0,
+  //     showinfo: 0,
+  //   },
+  // });
 
   $('.heroes__slider-img').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     asNavFor: '.heroes__slider-text',
+    prevArrow:
+      '<button class="slick-btn slick-prev"><img src="images/icon/arrow-left.png" alt="pre"> </button>',
+    nextArrow:
+      '<button class="slick-btn slick-next"><img src="images/icon/arrow-right.png" alt="next"> </button>',
   });
   $('.heroes__slider-text').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     asNavFor: '.heroes__slider-img',
     fade: true,
+    arrows: false,
   });
 
   // $('.carousel__inner').slick({
